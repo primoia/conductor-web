4dbfde2e615e8415907e974d0b1546984f2ea28d
Cezar Fuhr <primoia.dev@gmail.com>
2026-02-25 22:23:24 -0300

    feat(pwa): add service worker and update manifest for fullscreen mode

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
 import { DocumentCentricBackend } from './services/screenplay/document-centric.backend';
@@ -11,6 +12,10 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
     {
       provide: SCREENPLAY_BACKEND,
       useFactory: (docCentric: DocumentCentricBackend, eventSourced: EventSourcedBackend) => {
